3ca49beb476ab2dd6ca460db504fbf48ab8fcaa7
haui <hauitoo@googlemail.com>
2014-07-03 16:53:55 +0200

    maven project created

---
+++ jApiRecorder/src/main/java/haui/jApiRecorder/App.java
@@ -0,0 +1,13 @@
+package haui.jApiRecorder;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
